9681cb0c0c0c47cb450736233c47428f6b45bbdd
pramod kumar <prmd777@rediffmail.com>
2025-07-26 15:00:25 +0530

    infra code added and modified with storage account

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
 
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "webapp-rg"
   location = "West Europe"
+}
+
+resource "azurerm_storage_account" "aadu-storage" {
+  depends_on               = [azurerm_resource_group.example]
+  name                     = "aadustorageacct"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
